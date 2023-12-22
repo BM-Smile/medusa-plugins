1a49a399d5c85315d1a0f93dc8e7ffc1005440a1
BM-JiHoonPark <lucas@bmsmile.com>
2023-12-22 14:12:07 +0900

    COMMIT: Google, Kakao, Naver 제외 삭제

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -2,11 +2,6 @@ import { ConfigModule, MedusaContainer } from '@medusajs/medusa/dist/types/globa
 
 import { AuthOptions } from '../types';
 import GoogleStrategy from '../auth-strategies/google';
-import FacebookStrategy from '../auth-strategies/facebook';
-import LinkedinStrategy from '../auth-strategies/linkedin';
-import FireaseStrategy from '../auth-strategies/firebase';
-import Auth0Strategy from '../auth-strategies/auth0';
-import AzureStrategy from '../auth-strategies/azure-oidc';
 import KakaoStrategy from '../auth-strategies/kakao';
 import NaverStrategy from '../auth-strategies/naver';
 
@@ -14,11 +9,6 @@ export default async function authStrategiesLoader(container: MedusaContainer, a
 	const configModule = container.resolve('configModule') as ConfigModule;
 
 	GoogleStrategy.load(container, configModule, authOptions);
-	FacebookStrategy.load(container, configModule, authOptions);
-	LinkedinStrategy.load(container, configModule, authOptions);
-	FireaseStrategy.load(container, configModule, authOptions);
-	Auth0Strategy.load(container, configModule, authOptions);
-	AzureStrategy.load(container, configModule, authOptions);
 	KakaoStrategy.load(container, configModule, authOptions);
 	NaverStrategy.load(container, configModule, authOptions);
 }
